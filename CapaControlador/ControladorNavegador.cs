df4dcfccb1af8a19174c0db103def7e806054e62
Olstertecn11 <otzunund@miumg.edu.gt>
2023-09-04 19:57:07 -0600

    demostracion de comnicacion entre padre e hijo (navegador)

---
+++ CapaControlador/ControladorNavegador.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+namespace CapaControlador
+{
+    public class ControladorNavegador
+    {
+    }
+}
--- CapaVista/Navegador.cs
+++ CapaVista/Navegador.cs
@@ -7,14 +7,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CapaVista.Componentes;
+using CapaVista.Formularios;
 
 namespace CapaVista
 {
     public partial class Navegador : Form
     {
+
+        public Form frm_container;
+        public void openFormOnPanel(Panel panel, Form child)
+        {
+            child.Dock = DockStyle.Fill;
+            child.TopLevel = false;
+            panel.Controls.Add(child);
+            panel.Tag = child;
+            child.Show();
+        }
+
+        public void inicializador()
+        {
+            Reporte rpt = new Reporte();
+            this.frm_container = rpt;
+            this.openFormOnPanel(pnl_navegador, new Nav(this));
+            this.openFormOnPanel(pnl_contenedor, rpt);
+        }
+
         public Navegador()
         {
             InitializeComponent();
+            this.inicializador();
         }
     }
 }
