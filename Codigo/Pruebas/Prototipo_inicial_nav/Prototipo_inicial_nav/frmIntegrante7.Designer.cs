81c6aac23204aafdf8825dae28c7362c7e499552
JoshuaAguilar07 <98787109+JoshuaAguilar07@users.noreply.github.com>
2023-09-08 10:25:55 -0600

    Agregando integrante 7

--- Codigo/Pruebas/Prototipo_inicial_nav/Prototipo_inicial_nav/frmIntegrante7.Designer.cs
+++ Codigo/Pruebas/Prototipo_inicial_nav/Prototipo_inicial_nav/frmIntegrante7.Designer.cs
@@ -28,20 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(468, 185);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(203, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Joshua Erickson Aguilar Lopez";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(468, 306);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "9959-20-4660";
             // 
             // frmIntegrante7
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmIntegrante7";
             this.Text = "frmIntegrante7";
             this.Load += new System.EventHandler(this.frmIntegrante7_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
