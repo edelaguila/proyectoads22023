2b93d8c60cc2fee061d338d859005dc1538320fb
Julia Rashell <jlopezc121@miumg.edu.gt>
2023-09-10 22:04:38 -0600

    Agregando Diseño Funcional

---
+++ Codigo/Componentes/Reporteador/Reporteador-DiseñoFuncional/Reporteador/Vista de Reportes.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace Reporteador
+{
+    partial class Vista_de_Reportes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_titulo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_titulo
+            // 
+            this.lbl_titulo.AutoSize = true;
+            this.lbl_titulo.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Bold);
+            this.lbl_titulo.Location = new System.Drawing.Point(337, 20);
+            this.lbl_titulo.Name = "lbl_titulo";
+            this.lbl_titulo.Size = new System.Drawing.Size(98, 29);
+            this.lbl_titulo.TabIndex = 0;
+            this.lbl_titulo.Text = "Reporte";
+            // 
+            // Vista_de_Reportes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl_titulo);
+            this.Name = "Vista_de_Reportes";
+            this.Text = "Vista_de_Reportes";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_titulo;
+    }
+}
