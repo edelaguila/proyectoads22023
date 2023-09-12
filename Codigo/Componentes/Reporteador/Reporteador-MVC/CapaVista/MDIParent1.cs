9b371ba6786cc7eb1f32a5faac3d62376ae2eb23
Julia Rashell <jlopezc121@miumg.edu.gt>
2023-09-11 22:37:58 -0600

    Agregando MVC

---
+++ Codigo/Componentes/Reporteador/Reporteador-MVC/CapaVista/MDIParent1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Reporteador;
+
+namespace CapaVista
+{
+    public partial class MdiParent1 : Form
+    {
+        public MdiParent1()
+        {
+            InitializeComponent();
+        }
+
+        private void reportesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Reportes nuevo = new Reportes();
+            nuevo.MdiParent = this;
+            nuevo.Show();
+        }
+
+        private void navegadorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form1 nuevo = new Form1();
+            nuevo.MdiParent = this;
+            nuevo.Show();
+        }
+    }
+}
