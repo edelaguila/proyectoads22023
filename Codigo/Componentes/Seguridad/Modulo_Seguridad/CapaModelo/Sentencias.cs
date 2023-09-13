7708c090f659c172d3e905e62ed645dbb5ccd0db
JustinPennant <jramosp14@miumg.edu.gt>
2023-09-12 20:22:17 -0600

    Se añadio a CapaModelo/Sentencias funciones MODIFICAR

--- Codigo/Componentes/Seguridad/Modulo_Seguridad/CapaModelo/Sentencias.cs
+++ Codigo/Componentes/Seguridad/Modulo_Seguridad/CapaModelo/Sentencias.cs
@@ -13,7 +13,7 @@ namespace CapaModelo
     {
         Conexion con = new Conexion();
 
-     
+
         //carlos enrique
         public DataTable Buscar(string tabla, string columna, string dato)
         {
@@ -39,6 +39,163 @@ namespace CapaModelo
                 }
             }
         }
+
+
+
+        //FUNCIÓN PARA MODIFICAR
+        //-------------------------------------
+        // MODIFICACION DE PERFIL_ENCABEZADO
+        //-------------------------------------
+
+        void ModificarPerfilEncabezado(int idPerfil, string nuevoNombre, string nuevaDescripcion, int nuevoEstado)
+        {
+            string updateQuery = "UPDATE Seguridad.Tbl_Perfil_encabezado SET nombre_perfil = ?, descripcion_perfil = ?, estado_perfil = ? WHERE pk_id_perfil = ?";
+
+            using (OdbcConnection connection = con.conexion())
+            using (OdbcCommand consultaPerflEnc = new OdbcCommand(updateQuery, connection))
+            {
+                consultaPerflEnc.Parameters.AddWithValue("@nombre_perfil", nuevoNombre);
+                consultaPerflEnc.Parameters.AddWithValue("@descripcion_perfil", nuevaDescripcion);
+                consultaPerflEnc.Parameters.AddWithValue("@estado_perfil", nuevoEstado);
+                consultaPerflEnc.Parameters.AddWithValue("@pk_id_perfil", idPerfil);
+
+                try
+                {
+                    connection.Open();
+                    int rowsAffected = consultaPerflEnc.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+                        Console.WriteLine("Perfil modificado con éxito.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ningún perfil modificado. Verifica el identificador.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error al modificar el perfil: " + ex.Message);
+                }
+            }
+        }
+
+        //-------------------------------------
+        // MODIFICACION DE USUARIO_APLICACION
+        //-------------------------------------
+
+        void ModificarPerfilUsuarioAplicacion(int idUsuario, int idAplicacion, int ingresar, int consulta, int modificar, int eliminar, int imprimir)
+        {
+            string updateQuery = "UPDATE Seguridad.Tbl_Usuario_aplicacion SET ingresar = ?, consulta = ?, modificar = ?, eliminar = ?, imprimir = ? WHERE pk_id_usuario = ? AND pk_id_aplicacion = ?";
+
+            using (OdbcConnection connection = con.conexion())
+            using (OdbcCommand consultaPerUsuApp = new OdbcCommand(updateQuery, connection))
+            {
+                consultaPerUsuApp.Parameters.AddWithValue("@ingresar", ingresar);
+                consultaPerUsuApp.Parameters.AddWithValue("@consulta", consulta);
+                consultaPerUsuApp.Parameters.AddWithValue("@modificar", modificar);
+                consultaPerUsuApp.Parameters.AddWithValue("@eliminar", eliminar);
+                consultaPerUsuApp.Parameters.AddWithValue("@imprimir", imprimir);
+                consultaPerUsuApp.Parameters.AddWithValue("@pk_id_usuario", idUsuario);
+                consultaPerUsuApp.Parameters.AddWithValue("@pk_id_aplicacion", idAplicacion);
+
+                try
+                {
+                    connection.Open();
+                    int rowsAffected = consultaPerUsuApp.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+                        Console.WriteLine("Permisos de usuario modificados con éxito.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ningún permiso de usuario modificado. Verifica los identificadores.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error al modificar los permisos de usuario: " + ex.Message);
+                }
+            }
+        }
+
+
+        //-------------------------------------
+        // MODIFICACION DE PERFIL_DETALLE
+        //-------------------------------------
+
+        void ModificarPerfilDetalle(int idPerfil, int idAplicacion, int ingresar, int consultar, int modificar, int eliminar, int imprimir)
+        {
+            string updateQuery = "UPDATE Seguridad.Tbl_Perfil_detalle SET ingresar = ?, consultar = ?, modificar = ?, eliminar = ?, imprimir = ? WHERE pk_id_perfil = ? AND pk_id_aplicacion = ?";
+
+            using (OdbcConnection connection = con.conexion())
+            using (OdbcCommand consultaPerDetalle = new OdbcCommand(updateQuery, connection))
+            {
+                consultaPerDetalle.Parameters.AddWithValue("@ingresar", ingresar);
+                consultaPerDetalle.Parameters.AddWithValue("@consultar", consultar);
+                consultaPerDetalle.Parameters.AddWithValue("@modificar", modificar);
+                consultaPerDetalle.Parameters.AddWithValue("@eliminar", eliminar);
+                consultaPerDetalle.Parameters.AddWithValue("@imprimir", imprimir);
+                consultaPerDetalle.Parameters.AddWithValue("@pk_id_perfil", idPerfil);
+                consultaPerDetalle.Parameters.AddWithValue("@pk_id_aplicacion", idAplicacion);
+
+                try
+                {
+                    connection.Open();
+                    int rowsAffected = consultaPerDetalle.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+                        Console.WriteLine("Detalle de perfil modificado con éxito.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ningún detalle de perfil modificado. Verifica los identificadores.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+            }
+        }
+
+
+        //-------------------------------------
+        // MODIFICACION DE APLICACION A MODULO
+        //-------------------------------------
+
+        void ModificarAppModulo(int idAplicacion, int idModulo)
+        {
+            string updateQuery = "UPDATE Seguridad.Tbl_Aplicacion_a_modulos SET pk_id_modulo = ? WHERE pk_id_aplicacion = ?";
+
+            using (OdbcConnection connection = con.conexion())
+            using (OdbcCommand consultaAppModulo = new OdbcCommand(updateQuery, connection))
+            {
+                consultaAppModulo.Parameters.AddWithValue("@pk_id_modulo", idModulo);
+                consultaAppModulo.Parameters.AddWithValue("@pk_id_aplicacion", idAplicacion);
+
+                int rowsAffected = consultaAppModulo.ExecuteNonQuery();
+
+                try
+                {
+                    if (rowsAffected > 0)
+                    {
+                        Console.WriteLine("Asociación entre aplicación y módulo modificada con éxito.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ninguna asociación modificada. Verifica los identificadores.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+            }
+        }
+
 
 
     }
