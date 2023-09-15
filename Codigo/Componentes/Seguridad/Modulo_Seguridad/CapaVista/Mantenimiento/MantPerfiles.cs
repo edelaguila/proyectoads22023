40fb7c6ebfb1bb5717e0c417bc751a2999b9f503
Carlos Enrique <cguzmanc10@miumg.edu.gt>
2023-09-14 18:27:14 -0600

    cambio de nombre variables terminado siguiendo la estandarizacion acordada

--- Codigo/Componentes/Seguridad/Modulo_Seguridad/CapaVista/Mantenimiento/MantPerfiles.cs
+++ Codigo/Componentes/Seguridad/Modulo_Seguridad/CapaVista/Mantenimiento/MantPerfiles.cs
@@ -21,7 +21,7 @@ namespace CapaVista.Mantenimiento
         //carlos enrique
         public void Buscar()
         {
-            string dato = textBox1.Text;
+            string dato = txt_busqueda.Text;
             string tabla = "perfiles";
             string columna = "id_perfil";
             DataTable dt = cn.Buscar(tabla, columna, dato);
@@ -33,25 +33,25 @@ namespace CapaVista.Mantenimiento
                 DataRow row = dt.Rows[0]; // Tomamos la primera fila (si hay resultados)
 
                 // Llenamos los controles con los valores del resultado
-                textBox2.Text = row["id_perfil"].ToString();
-                textBox5.Text = row["nombre"].ToString();
-                textBox3.Text = row["descripcion"].ToString();
+                txt_codigo.Text = row["id_perfil"].ToString();
+                txt_nombre.Text = row["nombre"].ToString();
+                txt_descripcion.Text = row["descripcion"].ToString();
 
 
                 // Verificamos el estado y marcamos el RadioButton correspondiente
                 bool estadoActivo = Convert.ToInt32(row["estado"]) == 1;
-                radioButton1.Checked = estadoActivo;
-                radioButton2.Checked = !estadoActivo;
+                rb_habilitado.Checked = estadoActivo;
+                rb_inhabilitado.Checked = !estadoActivo;
             }
             else
             {
                 // Limpiamos los controles si no se encontraron resultados
-                textBox1.Text = string.Empty;
-                textBox2.Text = string.Empty;
-                textBox5.Text = string.Empty;
-                textBox3.Text = string.Empty;
-                radioButton1.Checked = false;
-                radioButton2.Checked = false;
+                txt_busqueda.Text = string.Empty;
+                txt_codigo.Text = string.Empty;
+                txt_nombre.Text = string.Empty;
+                txt_descripcion.Text = string.Empty;
+                rb_habilitado.Checked = false;
+                rb_inhabilitado.Checked = false;
             }
         }
 
