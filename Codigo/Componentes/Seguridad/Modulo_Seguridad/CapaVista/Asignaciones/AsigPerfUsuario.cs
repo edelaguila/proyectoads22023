d48a6202835f8b578efce3ed998e58c08079a7ba
unknown <jarriagac2@miumg.edu.gt>
2023-09-20 22:15:28 -0600

    Se termino ultima Asignacion

--- Codigo/Componentes/Seguridad/Modulo_Seguridad/CapaVista/Asignaciones/AsigPerfUsuario.cs
+++ Codigo/Componentes/Seguridad/Modulo_Seguridad/CapaVista/Asignaciones/AsigPerfUsuario.cs
@@ -16,6 +16,21 @@ namespace CapaVista.Asignaciones
         public AsigPerfUsuario()
         {
             InitializeComponent();
+            LlenarCombo();
+        }
+        Controlador cn = new Controlador();
+
+        public void LlenarCombo()
+        {
+            List<string> datos = cn.ObtenerDatos("nbr_username_usuario", "tbl_usuario");
+            List<string> datos2 = cn.ObtenerDatos("nbr_nombre_perfil", "tbl_perfil_encabezado");
+            comboBox1.Items.Clear();
+            comboBox1.Items.AddRange(datos.ToArray());
+            comboBox2.Items.Clear();
+            comboBox2.Items.AddRange(datos.ToArray());
+            comboBox3.Items.Clear();
+            comboBox3.Items.AddRange(datos2.ToArray());
+
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -25,25 +40,109 @@ namespace CapaVista.Asignaciones
 
         private void button2_Click(object sender, EventArgs e)
         {
-            string tabla = "NombreDeTuTabla";
-            Dictionary<string, object> valores = new Dictionary<string, object>();
-
-            Controlador controlador = new Controlador();
-            bool exito = controlador.GuardarDatos(tabla, valores);
-
-            if (exito)
-            {
-                MessageBox.Show("Los datos se han guardado correctamente.");
-            }
-            else
-            {
-                MessageBox.Show("Error al guardar los datos.");
-            }
+            int n = dataGridView2.Rows.Add();
+            dataGridView2.Rows[n].Cells[0].Value = comboBox2.SelectedItem;
+            dataGridView2.Rows[n].Cells[1].Value = comboBox3.SelectedItem;
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
             Help.ShowHelp(this, "ComponenteSeguridadAyuda/AyudaSeguridad.chm", "Procesos.html");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string FK_Usuario = "";
+            string FK_Perfil = "";
+            bool exito = true;
+            for (int i = 0; i < dataGridView2.Rows.Count; i++)
+            {
+                //BUSQUEDA USUARIO
+                string dato = Convert.ToString(dataGridView2.Rows[i].Cells["dgv_columna_Usuario2"].Value);
+                string tabla = "tbl_usuario";
+                string columna = "nbr_username_usuario";
+
+
+                DataTable dt = cn.Buscar(tabla, columna, dato);
+
+                if (dt.Rows.Count > 0)
+                {
+                    DataRow row = dt.Rows[0]; // Tomamos la primera fila (si hay resultados)
+                                              // Llenamos los controles con los valores del resultado
+                    FK_Usuario = row["PK_id_usuario"].ToString();
+                }
+                ///////////////
+                ///
+
+                //BUSQUEDA PERFIL
+
+                string dato2 = Convert.ToString(dataGridView2.Rows[i].Cells["dgv_columna_Perfil"].Value);
+                string tabla2 = "tbl_perfil_encabezado";
+                string columna2 = "nbr_nombre_perfil";
+                DataTable dt2 = cn.Buscar(tabla2, columna2, dato2);
+
+                if (dt.Rows.Count > 0)
+                {
+                    DataRow row = dt2.Rows[0]; // Tomamos la primera fila (si hay resultados)
+                                               // Llenamos los controles con los valores del resultado
+                    FK_Perfil = row["PK_id_perfil"].ToString();
+                }
+
+                ////////////////////////
+                ///
+
+                // INGRESO DE DATOS A TABLA TBL_USUARIO_APLICACION
+                string tabla1 = "tbl_usuario_perfil";
+                Dictionary<string, object> valores = new Dictionary<string, object>();
+                Controlador controlador = new Controlador();
+
+                valores.Add("FK_id_usuario", FK_Usuario);
+                valores.Add("FK_id_perfil", FK_Perfil);
+
+                exito = controlador.GuardarDatos(tabla1, valores);
+
+
+            }
+            if (exito == true)
+            {
+                MessageBox.Show("Los datos se han guardado correctamente");
+            }
+            else
+            {
+                MessageBox.Show("Error al guardar los datos");
+            }
+
+            dataGridView2.Rows.Clear();
+            comboBox2.Text = "";
+            comboBox3.Text = "";
+            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string id = "";
+            string valor = comboBox1.SelectedItem.ToString();
+
+
+            // BUSQUEDA USUARIO
+            string dato = valor;
+            string tabla = "tbl_usuario";
+            string columna = "nbr_username_usuario";
+
+
+            DataTable dt = cn.Buscar(tabla, columna, dato);
+
+            if (dt.Rows.Count > 0)
+            {
+                DataRow row = dt.Rows[0]; // Tomamos la primera fila (si hay resultados)
+                                          // Llenamos los controles con los valores del resultado
+                id = row["PK_id_usuario"].ToString();
+            }
+
+            dataGridView1.DataSource = cn.Llenardatagrid(id);
+            dataGridView1.Columns["ID_Perfil_Usuario"].HeaderText = "Codigo";
+            dataGridView1.Columns["Nombre_Perfil"].HeaderText = "Perfil";
+            
         }
     }
 }
