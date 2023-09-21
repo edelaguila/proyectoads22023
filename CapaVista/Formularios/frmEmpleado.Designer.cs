3552201f7a2ad3d4a88f966ba77e1420f75b2261
coradoa <acoradop@miumg.edu.gt>
2023-09-20 19:39:23 -0600

    Agregado empleado

---
+++ CapaVista/Formularios/frmEmpleado.Designer.cs
@@ -0,0 +1,277 @@
+﻿
+namespace CapaVista.Formularios
+{
+    partial class frmEmpleado
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_Id_emp = new System.Windows.Forms.TextBox();
+            this.txt_nombre = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_apellido_emp = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_fecha_na = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txt_puesto = new System.Windows.Forms.TextBox();
+            this.txt_dirreccion_e = new System.Windows.Forms.TextBox();
+            this.txt_sueldo = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txt_correo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(239, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(279, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Registro de Empleados";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(83, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ID empleado";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(83, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nombre";
+            // 
+            // txt_Id_emp
+            // 
+            this.txt_Id_emp.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Id_emp.Location = new System.Drawing.Point(278, 78);
+            this.txt_Id_emp.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt_Id_emp.Name = "txt_Id_emp";
+            this.txt_Id_emp.Size = new System.Drawing.Size(160, 26);
+            this.txt_Id_emp.TabIndex = 3;
+            this.txt_Id_emp.Tag = "pk_id_empleado";
+            // 
+            // txt_nombre
+            // 
+            this.txt_nombre.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_nombre.Location = new System.Drawing.Point(278, 116);
+            this.txt_nombre.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt_nombre.Name = "txt_nombre";
+            this.txt_nombre.Size = new System.Drawing.Size(160, 26);
+            this.txt_nombre.TabIndex = 4;
+            this.txt_nombre.Tag = "nbr_nombre_empleado";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(82, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Apellido";
+            // 
+            // txt_apellido_emp
+            // 
+            this.txt_apellido_emp.Location = new System.Drawing.Point(278, 149);
+            this.txt_apellido_emp.Name = "txt_apellido_emp";
+            this.txt_apellido_emp.Size = new System.Drawing.Size(160, 26);
+            this.txt_apellido_emp.TabIndex = 6;
+            this.txt_apellido_emp.Tag = "nbr_apellido_empleado";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(83, 183);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(165, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Fecha de nacimiento";
+            // 
+            // txt_fecha_na
+            // 
+            this.txt_fecha_na.Location = new System.Drawing.Point(278, 183);
+            this.txt_fecha_na.Name = "txt_fecha_na";
+            this.txt_fecha_na.Size = new System.Drawing.Size(160, 26);
+            this.txt_fecha_na.TabIndex = 8;
+            this.txt_fecha_na.Tag = "nbr_fecha_nacimiento_e";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(83, 222);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 20);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Puesto";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(536, 78);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(80, 20);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Dirección";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(536, 116);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(58, 20);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Sueldo";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(536, 149);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(71, 20);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "Teléfono";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(536, 183);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(61, 20);
+            this.label10.TabIndex = 13;
+            this.label10.Text = "Correo";
+            // 
+            // txt_puesto
+            // 
+            this.txt_puesto.Location = new System.Drawing.Point(278, 219);
+            this.txt_puesto.Name = "txt_puesto";
+            this.txt_puesto.Size = new System.Drawing.Size(160, 26);
+            this.txt_puesto.TabIndex = 14;
+            this.txt_puesto.Tag = "nbr_puesto_empleado";
+            // 
+            // txt_dirreccion_e
+            // 
+            this.txt_dirreccion_e.Location = new System.Drawing.Point(622, 75);
+            this.txt_dirreccion_e.Name = "txt_dirreccion_e";
+            this.txt_dirreccion_e.Size = new System.Drawing.Size(160, 26);
+            this.txt_dirreccion_e.TabIndex = 15;
+            this.txt_dirreccion_e.Tag = "nbr_direccion_empleado";
+            // 
+            // txt_sueldo
+            // 
+            this.txt_sueldo.Location = new System.Drawing.Point(622, 113);
+            this.txt_sueldo.Name = "txt_sueldo";
+            this.txt_sueldo.Size = new System.Drawing.Size(160, 26);
+            this.txt_sueldo.TabIndex = 16;
+            this.txt_sueldo.Tag = "nbr_sueldo_empleado";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(622, 145);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(160, 26);
+            this.textBox1.TabIndex = 17;
+            this.textBox1.Tag = "nbr_telefono_empleado";
+            // 
+            // txt_correo
+            // 
+            this.txt_correo.Location = new System.Drawing.Point(622, 177);
+            this.txt_correo.Name = "txt_correo";
+            this.txt_correo.Size = new System.Drawing.Size(160, 26);
+            this.txt_correo.TabIndex = 18;
+            this.txt_correo.Tag = "nbr_correo_empleado";
+            // 
+            // frmEmpleado
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(839, 269);
+            this.Controls.Add(this.txt_correo);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txt_sueldo);
+            this.Controls.Add(this.txt_dirreccion_e);
+            this.Controls.Add(this.txt_puesto);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txt_fecha_na);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txt_apellido_emp);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_nombre);
+            this.Controls.Add(this.txt_Id_emp);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "frmEmpleado";
+            this.Text = "frmEmpleado";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_Id_emp;
+        private System.Windows.Forms.TextBox txt_nombre;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_apellido_emp;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_fecha_na;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txt_puesto;
+        private System.Windows.Forms.TextBox txt_dirreccion_e;
+        private System.Windows.Forms.TextBox txt_sueldo;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txt_correo;
+    }
+}
