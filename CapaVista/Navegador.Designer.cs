69150c167321b0484fcee71e8c12555f63f4b68d
D4nnny <dperezf5@miumg.edu.gt>
2023-09-22 00:59:57 -0600

    Agregando funcionalidad Inicio y Fin

--- CapaVista/Navegador.Designer.cs
+++ CapaVista/Navegador.Designer.cs
@@ -63,9 +63,9 @@
             this.panel.Controls.Add(this.btn_modificar);
             this.panel.Controls.Add(this.btn_agregar);
             this.panel.Location = new System.Drawing.Point(0, 0);
-            this.panel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel.Name = "panel";
-            this.panel.Size = new System.Drawing.Size(1001, 96);
+            this.panel.Size = new System.Drawing.Size(751, 78);
             this.panel.TabIndex = 1;
             // 
             // btn_ayuda
@@ -75,10 +75,10 @@
             this.btn_ayuda.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_ayuda.Image = ((System.Drawing.Image)(resources.GetObject("btn_ayuda.Image")));
             this.btn_ayuda.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.btn_ayuda.Location = new System.Drawing.Point(791, 12);
-            this.btn_ayuda.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_ayuda.Location = new System.Drawing.Point(593, 10);
+            this.btn_ayuda.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_ayuda.Name = "btn_ayuda";
-            this.btn_ayuda.Size = new System.Drawing.Size(92, 69);
+            this.btn_ayuda.Size = new System.Drawing.Size(69, 56);
             this.btn_ayuda.TabIndex = 12;
             this.btn_ayuda.Text = "Ayuda";
             this.btn_ayuda.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -91,13 +91,14 @@
             this.btn_fin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_fin.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_fin.Image = ((System.Drawing.Image)(resources.GetObject("btn_fin.Image")));
-            this.btn_fin.Location = new System.Drawing.Point(940, 12);
-            this.btn_fin.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_fin.Location = new System.Drawing.Point(705, 10);
+            this.btn_fin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_fin.Name = "btn_fin";
-            this.btn_fin.Size = new System.Drawing.Size(47, 33);
+            this.btn_fin.Size = new System.Drawing.Size(35, 27);
             this.btn_fin.TabIndex = 11;
             this.btn_fin.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.btn_fin.UseVisualStyleBackColor = false;
+            this.btn_fin.Click += new System.EventHandler(this.btn_fin_Click);
             // 
             // btn_siguiente
             // 
@@ -105,13 +106,14 @@
             this.btn_siguiente.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_siguiente.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_siguiente.Image = ((System.Drawing.Image)(resources.GetObject("btn_siguiente.Image")));
-            this.btn_siguiente.Location = new System.Drawing.Point(940, 48);
-            this.btn_siguiente.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_siguiente.Location = new System.Drawing.Point(705, 39);
+            this.btn_siguiente.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_siguiente.Name = "btn_siguiente";
-            this.btn_siguiente.Size = new System.Drawing.Size(47, 33);
+            this.btn_siguiente.Size = new System.Drawing.Size(35, 27);
             this.btn_siguiente.TabIndex = 10;
             this.btn_siguiente.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.btn_siguiente.UseVisualStyleBackColor = false;
+            this.btn_siguiente.Click += new System.EventHandler(this.btn_siguiente_Click);
             // 
             // btn_anterior
             // 
@@ -119,13 +121,14 @@
             this.btn_anterior.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_anterior.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_anterior.Image = ((System.Drawing.Image)(resources.GetObject("btn_anterior.Image")));
-            this.btn_anterior.Location = new System.Drawing.Point(888, 48);
-            this.btn_anterior.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_anterior.Location = new System.Drawing.Point(666, 39);
+            this.btn_anterior.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_anterior.Name = "btn_anterior";
-            this.btn_anterior.Size = new System.Drawing.Size(47, 33);
+            this.btn_anterior.Size = new System.Drawing.Size(35, 27);
             this.btn_anterior.TabIndex = 9;
             this.btn_anterior.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.btn_anterior.UseVisualStyleBackColor = false;
+            this.btn_anterior.Click += new System.EventHandler(this.btn_anterior_Click);
             // 
             // btn_inicio
             // 
@@ -133,13 +136,14 @@
             this.btn_inicio.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_inicio.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_inicio.Image = ((System.Drawing.Image)(resources.GetObject("btn_inicio.Image")));
-            this.btn_inicio.Location = new System.Drawing.Point(888, 12);
-            this.btn_inicio.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_inicio.Location = new System.Drawing.Point(666, 10);
+            this.btn_inicio.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_inicio.Name = "btn_inicio";
-            this.btn_inicio.Size = new System.Drawing.Size(47, 33);
+            this.btn_inicio.Size = new System.Drawing.Size(35, 27);
             this.btn_inicio.TabIndex = 8;
             this.btn_inicio.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.btn_inicio.UseVisualStyleBackColor = false;
+            this.btn_inicio.Click += new System.EventHandler(this.btn_inicio_Click);
             // 
             // btn_refrescar
             // 
@@ -148,10 +152,10 @@
             this.btn_refrescar.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_refrescar.Image = ((System.Drawing.Image)(resources.GetObject("btn_refrescar.Image")));
             this.btn_refrescar.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.btn_refrescar.Location = new System.Drawing.Point(693, 14);
-            this.btn_refrescar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_refrescar.Location = new System.Drawing.Point(520, 11);
+            this.btn_refrescar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_refrescar.Name = "btn_refrescar";
-            this.btn_refrescar.Size = new System.Drawing.Size(92, 68);
+            this.btn_refrescar.Size = new System.Drawing.Size(69, 55);
             this.btn_refrescar.TabIndex = 7;
             this.btn_refrescar.Text = "Refrescar";
             this.btn_refrescar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -165,10 +169,10 @@
             this.btn_imprimir.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_imprimir.Image = ((System.Drawing.Image)(resources.GetObject("btn_imprimir.Image")));
             this.btn_imprimir.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.btn_imprimir.Location = new System.Drawing.Point(596, 14);
-            this.btn_imprimir.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_imprimir.Location = new System.Drawing.Point(447, 11);
+            this.btn_imprimir.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_imprimir.Name = "btn_imprimir";
-            this.btn_imprimir.Size = new System.Drawing.Size(92, 68);
+            this.btn_imprimir.Size = new System.Drawing.Size(69, 55);
             this.btn_imprimir.TabIndex = 6;
             this.btn_imprimir.Text = "Imprimir";
             this.btn_imprimir.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -181,10 +185,10 @@
             this.btn_eliminar.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_eliminar.Image = ((System.Drawing.Image)(resources.GetObject("btn_eliminar.Image")));
             this.btn_eliminar.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.btn_eliminar.Location = new System.Drawing.Point(401, 14);
-            this.btn_eliminar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_eliminar.Location = new System.Drawing.Point(301, 11);
+            this.btn_eliminar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_eliminar.Name = "btn_eliminar";
-            this.btn_eliminar.Size = new System.Drawing.Size(92, 68);
+            this.btn_eliminar.Size = new System.Drawing.Size(69, 55);
             this.btn_eliminar.TabIndex = 5;
             this.btn_eliminar.Text = "Eliminar";
             this.btn_eliminar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -197,10 +201,10 @@
             this.btn_consultar.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_consultar.Image = ((System.Drawing.Image)(resources.GetObject("btn_consultar.Image")));
             this.btn_consultar.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.btn_consultar.Location = new System.Drawing.Point(499, 14);
-            this.btn_consultar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_consultar.Location = new System.Drawing.Point(374, 11);
+            this.btn_consultar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_consultar.Name = "btn_consultar";
-            this.btn_consultar.Size = new System.Drawing.Size(92, 68);
+            this.btn_consultar.Size = new System.Drawing.Size(69, 55);
             this.btn_consultar.TabIndex = 4;
             this.btn_consultar.Text = "Consultar";
             this.btn_consultar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -213,10 +217,10 @@
             this.btn_cancelar.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_cancelar.Image = ((System.Drawing.Image)(resources.GetObject("btn_cancelar.Image")));
             this.btn_cancelar.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.btn_cancelar.Location = new System.Drawing.Point(304, 14);
-            this.btn_cancelar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_cancelar.Location = new System.Drawing.Point(228, 11);
+            this.btn_cancelar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_cancelar.Name = "btn_cancelar";
-            this.btn_cancelar.Size = new System.Drawing.Size(92, 68);
+            this.btn_cancelar.Size = new System.Drawing.Size(69, 55);
             this.btn_cancelar.TabIndex = 3;
             this.btn_cancelar.Text = "Cancelar";
             this.btn_cancelar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -230,10 +234,10 @@
             this.btn_guardar.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_guardar.Image = ((System.Drawing.Image)(resources.GetObject("btn_guardar.Image")));
             this.btn_guardar.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.btn_guardar.Location = new System.Drawing.Point(207, 14);
-            this.btn_guardar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_guardar.Location = new System.Drawing.Point(155, 11);
+            this.btn_guardar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_guardar.Name = "btn_guardar";
-            this.btn_guardar.Size = new System.Drawing.Size(92, 68);
+            this.btn_guardar.Size = new System.Drawing.Size(69, 55);
             this.btn_guardar.TabIndex = 2;
             this.btn_guardar.Text = "Guardar";
             this.btn_guardar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -247,10 +251,10 @@
             this.btn_modificar.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_modificar.Image = ((System.Drawing.Image)(resources.GetObject("btn_modificar.Image")));
             this.btn_modificar.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.btn_modificar.Location = new System.Drawing.Point(109, 14);
-            this.btn_modificar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_modificar.Location = new System.Drawing.Point(82, 11);
+            this.btn_modificar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_modificar.Name = "btn_modificar";
-            this.btn_modificar.Size = new System.Drawing.Size(92, 68);
+            this.btn_modificar.Size = new System.Drawing.Size(69, 55);
             this.btn_modificar.TabIndex = 1;
             this.btn_modificar.Text = "Modificar";
             this.btn_modificar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -264,10 +268,10 @@
             this.btn_agregar.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_agregar.Image = ((System.Drawing.Image)(resources.GetObject("btn_agregar.Image")));
             this.btn_agregar.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.btn_agregar.Location = new System.Drawing.Point(12, 14);
-            this.btn_agregar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_agregar.Location = new System.Drawing.Point(9, 11);
+            this.btn_agregar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_agregar.Name = "btn_agregar";
-            this.btn_agregar.Size = new System.Drawing.Size(92, 68);
+            this.btn_agregar.Size = new System.Drawing.Size(69, 55);
             this.btn_agregar.TabIndex = 0;
             this.btn_agregar.Text = "Agregar";
             this.btn_agregar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -276,12 +280,11 @@
             // 
             // Navegador
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.panel);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Navegador";
-            this.Size = new System.Drawing.Size(1001, 96);
+            this.Size = new System.Drawing.Size(751, 78);
             this.panel.ResumeLayout(false);
             this.ResumeLayout(false);
 
