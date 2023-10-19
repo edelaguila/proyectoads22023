defefb067dbaddb28d79a10bd3c49266f59425d1
GustavoAZepeda <gzepedah2@miumg.edu.gt>
2023-10-18 19:14:15 -0600

    Interfaz Mantenimientos

---
+++ Codigo/Modulos/Nominas/NOFUNCIONAL/NominaFinal/CapaVista/frm_mantenimiento.Designer.cs
@@ -0,0 +1,169 @@
+﻿
+namespace CapaVista
+{
+    partial class frm_mantenimiento
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_sueldoB_empleado = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_apellido_empleado = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_nombre_empleado = new System.Windows.Forms.TextBox();
+            this.txt_id_empleado = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txt_sueldoB_empleado
+            // 
+            this.txt_sueldoB_empleado.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_sueldoB_empleado.Location = new System.Drawing.Point(289, 187);
+            this.txt_sueldoB_empleado.Name = "txt_sueldoB_empleado";
+            this.txt_sueldoB_empleado.Size = new System.Drawing.Size(234, 34);
+            this.txt_sueldoB_empleado.TabIndex = 53;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(38, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(247, 30);
+            this.label4.TabIndex = 52;
+            this.label4.Text = "Apellido_Empleado";
+            // 
+            // txt_apellido_empleado
+            // 
+            this.txt_apellido_empleado.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_apellido_empleado.Location = new System.Drawing.Point(289, 136);
+            this.txt_apellido_empleado.Name = "txt_apellido_empleado";
+            this.txt_apellido_empleado.Size = new System.Drawing.Size(234, 34);
+            this.txt_apellido_empleado.TabIndex = 51;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(38, 187);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 30);
+            this.label3.TabIndex = 50;
+            this.label3.Text = "Cargo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(39, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(245, 30);
+            this.label2.TabIndex = 49;
+            this.label2.Text = "Nombre_Empleado";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(38, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 30);
+            this.label1.TabIndex = 48;
+            this.label1.Text = "ID_Empleado";
+            // 
+            // txt_nombre_empleado
+            // 
+            this.txt_nombre_empleado.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_nombre_empleado.Location = new System.Drawing.Point(289, 85);
+            this.txt_nombre_empleado.Name = "txt_nombre_empleado";
+            this.txt_nombre_empleado.Size = new System.Drawing.Size(234, 34);
+            this.txt_nombre_empleado.TabIndex = 47;
+            // 
+            // txt_id_empleado
+            // 
+            this.txt_id_empleado.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_id_empleado.Location = new System.Drawing.Point(289, 34);
+            this.txt_id_empleado.Name = "txt_id_empleado";
+            this.txt_id_empleado.Size = new System.Drawing.Size(127, 34);
+            this.txt_id_empleado.TabIndex = 46;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(38, 238);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(213, 30);
+            this.label5.TabIndex = 54;
+            this.label5.Text = "Acción a realizar";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(289, 238);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(234, 34);
+            this.textBox1.TabIndex = 55;
+            // 
+            // frm_mantenimiento
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(696, 329);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txt_sueldoB_empleado);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_apellido_empleado);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_nombre_empleado);
+            this.Controls.Add(this.txt_id_empleado);
+            this.Name = "frm_mantenimiento";
+            this.Text = "frm_mantenimiento";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_sueldoB_empleado;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_apellido_empleado;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_nombre_empleado;
+        private System.Windows.Forms.TextBox txt_id_empleado;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
