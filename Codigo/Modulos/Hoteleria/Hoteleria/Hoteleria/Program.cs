e3ab6a36f1bcceeb37011c934c5f4aa8b7d72421
Olstertecn11 <otzunund@miumg.edu.gt>
2023-10-25 23:42:47 -0600

    Agregando MDI

--- Codigo/Modulos/Hoteleria/Hoteleria/Hoteleria/Program.cs
+++ Codigo/Modulos/Hoteleria/Hoteleria/Hoteleria/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Vista_PrototipoMenu;
 
 namespace Hoteleria
 {
@@ -16,7 +17,7 @@ namespace Hoteleria
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new GestionHabitaciones());
+            Application.Run(new MDIHoteleria());
         }
     }
 }
