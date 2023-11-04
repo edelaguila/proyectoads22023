bb68c72a82c35fa773877e4f4f2f69b14b3fb79f
carlos enrique <cguzmanc10@miumg.edu.gt>
2023-11-03 22:55:47 -0600

    se desactiva temporalmente seguridad debido a un error

--- Codigo/Modulos/Administracion/VentasCxc/CapaVista/MDIVentasCXC.cs
+++ Codigo/Modulos/Administracion/VentasCxc/CapaVista/MDIVentasCXC.cs
@@ -22,15 +22,15 @@ namespace CapaVista
             Button[] apps = {btnclientes, btnconcepto, btnmovimientocliente, btncotizaciones, btnpedido, btnfactura};
             //Llamada metodo de libreria Controlador del modulo de Seguridad
 
-            cns.deshabilitarApps(apps);
+           // cns.deshabilitarApps(apps);
             //Llamada metodo de libreria Controlador del modulo de Seguridad
 
-            cns.getAccesoApp(3001, apps[1]);
-            cns.getAccesoApp(3002, apps[2]);
-            cns.getAccesoApp(3003, apps[3]);
-            cns.getAccesoApp(3004, apps[4]);
-            cns.getAccesoApp(3005, apps[5]);
-            cns.getAccesoApp(3006, apps[6]);
+            //cns.getAccesoApp(3001, apps[1]);
+            //cns.getAccesoApp(3002, apps[2]);
+            //cns.getAccesoApp(3003, apps[3]);
+            //cns.getAccesoApp(3004, apps[4]);
+            //cns.getAccesoApp(3005, apps[5]);
+            //cns.getAccesoApp(3006, apps[6]);
         }
 
         //Validaciones que si son visibles los panales los oculta
