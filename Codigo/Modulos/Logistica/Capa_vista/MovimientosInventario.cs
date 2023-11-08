bd160b6b7d106a34d1a36467d1be523d26301d39
D4nnny <dperezf5@miumg.edu.gt>
2023-11-07 18:36:15 -0600

    Prototipo 100% Funcional

--- Codigo/Modulos/Logistica/Capa_vista/MovimientosInventario.cs
+++ Codigo/Modulos/Logistica/Capa_vista/MovimientosInventario.cs
@@ -22,80 +22,6 @@ namespace Vista_PrototipoMenu
             InitializeComponent();
         }
 
-        private void btn_aceptar_Click(object sender, EventArgs e)
-        {
-            // BOTON ACEPTAR, PARA MANDAR LOS DATOS DE LOS TXT AL DATAGRIDVIEW
-            
-
-            // Obtén los valores de los TextBox
-            string valor1 = txt_id.Text;
-            string valor2 = txt_almacen.Text;
-            string valor3 = txt_movimiento.Text;
-            string valor4 = txt_doc.Text;
-            string valor5 = txt_fecha.Text;
-            string valor6 = txt_codigos.Text;
-            string valor7 = txt_medida.Text;
-            string valor8 = txt_cantidad.Text;
-            string valor9 = txt_costo.Text;
-            string valor10 = txt_precio.Text;
-         
-
-            // Añade los valores al DataGridView
-            dgv_inventario.Rows.Add(valor1, valor2, valor3, valor4, valor5, valor6, valor7, valor8, valor9, valor10);
-
-            // Limpia los TextBox después de agregar los datos al DataGridView
-            txt_id.Text = "";
-            txt_almacen.Text = "";
-            txt_movimiento.Text = "";
-            txt_doc.Text = "";
-            txt_fecha.Text = "";
-            txt_codigos.Text = "";
-            txt_medida.Text = "";
-            txt_cantidad.Text = "";
-            txt_cantidad.Text = "";
-            txt_costo.Text = "";
-            txt_precio.Text = "";
-            
-        }
-
-        private void btn_quitar_Click(object sender, EventArgs e)
-        {
-            // BOTON ACEPTAR, PARA MANDAR LOS DATOS DE LOS TXT AL DATAGRIDVIEW
-            // Justin Emmanuel Ramos Pennant
-
-            // Verifica si hay al menos una fila seleccionada en el DataGridView
-            if (dgv_inventario.SelectedRows.Count > 0)
-            {
-                // Recorre las filas seleccionadas en reversa para evitar problemas con los índices
-                for (int i = dgv_inventario.SelectedRows.Count - 1; i >= 0; i--)
-                {
-                    dgv_inventario.Rows.RemoveAt(dgv_inventario.SelectedRows[i].Index);
-                }
-            }
-            else
-            {
-                // Muestra un mensaje si no se ha seleccionado ninguna fila
-                MessageBox.Show("Seleccione uno o varios datos antes de remover", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
-        }
-
-    
-
-         private void navegador2_Load(object sender, EventArgs e)
-        {
-
-            NavegadorVista.Navegador.idApp = "4001";  //código aplicación asignado al formulario
-            navegador2.actual = this;
-            navegador2.tabla = dgv_inventario;
-            TextBox[] Grupotextbox = { txt_id, txt_almacen, txt_movimiento, txt_doc, txt_fecha, txt_codigos, txt_medida,txt_cantidad,
-            txt_costo, txt_precio};
-            TextBox[] Idtextbox = { txt_id, txt_codigos};
-            navegador2.textbox = Grupotextbox;
-            navegador2.textboxi = Idtextbox;
-            navegador2.cargar(dgv_inventario, Grupotextbox, cn.getNombreBd());
-        
-        }
-
         private void txt_doc_TextChanged(object sender, EventArgs e)
         {
 
@@ -103,6 +29,52 @@ namespace Vista_PrototipoMenu
 
         private void label3_Click(object sender, EventArgs e)
         {
+
+        }
+
+        private void navegador3_Load(object sender, EventArgs e)
+        {
+            NavegadorVista.Navegador.idApp = "4001";  //código aplicación asignado al formulario
+            navegador3.actual = this;
+            navegador3.tabla = dgv_inventario;
+            TextBox[] Grupotextbox = { txt_id, txt_almacen, txt_movimiento, txt_doc, txt_fecha, txt_codigos, txt_medida,txt_cantidad,
+            txt_costo, txt_precio};
+            TextBox[] Idtextbox = { txt_id, txt_almacen };
+            navegador3.textbox = Grupotextbox;
+            navegador3.textboxi = Idtextbox;
+            navegador3.cargar(dgv_inventario, Grupotextbox, cn.getNombreBd());
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string textoSeleccionado = cmb_tipomov.SelectedItem.ToString();
+            txt_movimiento.Text = textoSeleccionado;
+        }
+
+        private void label15_Click(object sender, EventArgs e)
+        {
+           
+
+
+        }
+
+        private void MovimientosInventario_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btn_suma_Click(object sender, EventArgs e)
+        {
+            double suma2 = 0;
+            double suma = 0;
+            for (int i = 0; i < dgv_inventario.Rows.Count; i++)
+            {
+                suma += Convert.ToDouble(dgv_inventario.Rows[i].Cells["mov_costo"].Value);
+                suma2 += Convert.ToDouble(dgv_inventario.Rows[i].Cells["mov_preciou"].Value);
+            }
+
+            label15.Text = suma.ToString();
+            label16.Text = suma2.ToString();
 
         }
     }
