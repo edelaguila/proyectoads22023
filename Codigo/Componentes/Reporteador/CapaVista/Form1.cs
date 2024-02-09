f8483099c3036e1a9949a80b5438dd49509a2d5a
Esduardo Del Aguila <daguilae@yahoo.com>
2024-02-09 08:24:45 -0600

    Reporteador

---
+++ Codigo/Componentes/Reporteador/CapaVista/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CapaControlador;
+
+
+namespace Reporteador
+{
+    public partial class Form1 : Form
+    {
+        
+        Controlador cn = new Controlador();
+        string app = "tbl_aplicacion";
+        
+        public Form1()
+        {
+            
+            InitializeComponent();
+            actualizardatagriew();
+        }
+        public void actualizardatagriew()
+        {
+            DataTable dt = cn.llenarTblapl(app);
+            dgv_aplicacion.DataSource = dt;
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void btn_imprimir_Click(object sender, EventArgs e)
+        {
+            if (dgv_aplicacion.SelectedRows.Count > 0)
+            {
+                int correlativo = Convert.ToInt32(dgv_aplicacion.SelectedRows[0].Cells["fk_no_reporteAsociado"].Value);
+
+                string rutaInforme = cn.ObtenerRutaInformePorCorrelativo(correlativo);
+
+                if (!string.IsNullOrEmpty(rutaInforme))
+                {
+                    CapaVista.VistaNav vistaReportesNav = new CapaVista.VistaNav();
+                    vistaReportesNav.CargarInforme(rutaInforme);
+                    vistaReportesNav.Show();
+                }
+                else
+                {
+                    MessageBox.Show("No se encontró la ruta del informe para este correlativo.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor, seleccione una aplicación en el DataGridView para ver el informe.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void dgv_categorias_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Codigo/Componentes/Reporteador/CapaVista/MDIParent1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Reporteador;
+
+namespace CapaVista
+{
+    public partial class MdiParent1 : Form
+    {
+        public MdiParent1()
+        {
+            InitializeComponent();
+        }
+
+        private void reportesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void navegadorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form1 nuevo = new Form1();
+            nuevo.MdiParent = this;
+            nuevo.Show();
+        }
+
+        private void reporte1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void reporte2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Reportes nuevo = new Reportes();
+            nuevo.MdiParent = this;
+            nuevo.Show();
+        }
+
+        private void reporte1ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            prubacrystal prueba = new prubacrystal();
+            prueba.MdiParent = this;
+            prueba.Show();
+
+        }
+
+        private void reporte2ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            prueba2 prueba = new prueba2();
+            prueba.MdiParent = this;
+            prueba.Show();
+        }
+
+        private void reporte3ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Plantilla3 prueba = new Plantilla3();
+            prueba.MdiParent = this;
+            prueba.Show();
+        }
+
+        private void reporte4ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Plantilla4 prueba = new Plantilla4();
+            prueba.MdiParent = this;
+            prueba.Show();
+        }
+    }
+}
